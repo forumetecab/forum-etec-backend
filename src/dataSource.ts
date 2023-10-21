784ca3584c59faca25870caa172bd39a677517bc
leeool <lgonsalez02@gmail.com>
2023-10-21 18:17:05 -0300

    feat: update database connection method

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,19 +1,15 @@
-import { DataSource } from "typeorm"
-import "dotenv/config"
-import "reflect-metadata"
+import { DataSource } from "typeorm";
+import "dotenv/config";
+import "reflect-metadata";
 
 const appDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  url: process.env.DB_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
   },
   entities: [__dirname + "/**/entities/*.{js,ts}"],
-  migrations: [__dirname + "/**/migrations/*.{js,ts}"]
-})
+  migrations: [__dirname + "/**/migrations/*.{js,ts}"],
+});
 
-export default appDataSource
+export default appDataSource;
